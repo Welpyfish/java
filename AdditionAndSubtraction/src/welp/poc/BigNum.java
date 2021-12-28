6aa339f3063be78d9415be5fec35801e4372d883
William <williamzhou2015@gmail.com>
2021-12-28 15:19:47 -0500

    Completed adding and subtracting big numbers

--- AdditionAndSubtraction/src/welp/poc/BigNum.java
+++ AdditionAndSubtraction/src/welp/poc/BigNum.java
@@ -40,7 +40,33 @@ public class BigNum {
     }
 
     public void setValue(String num){
-        setValue("+", num);
+        if (num != null && num.trim().length() > 0) {
+            boolean isValid = true;
+            for(int i=0; i < num.length(); i++) {
+                if (i == 0 && (num.charAt(i) == '+' || num.charAt(i) == '-')) {
+                    continue;
+                }
+                if (num.charAt(i) > '9' || num.charAt(i) < '0' ) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid) {
+                if (num.charAt(0) == '-') {
+                    numSign = "-";
+                    numString = num.substring(1);
+                } else if (num.charAt(0) == '+') {
+                    numSign = "+";
+                    numString = num.substring(1);
+                } else {
+                    numString = num;
+                }
+            } else {
+                throw new IllegalArgumentException("Invalid number: big number has invalid character.");
+            }
+        } else {
+            throw new IllegalArgumentException("Invalid number: big number cannot be empty.");
+        }
     }
 
     public String getNumSign(){
@@ -49,5 +75,27 @@ public class BigNum {
 
     public String getNumString() {
         return numString;
+    }
+    public void switchSign(){
+        if(numSign == "+"){
+            numSign = "-";
+        } else {
+            numSign = "+";
+        }
+    }
+
+    public String getValue() {
+        String result = (numSign == "-" ? numSign : "") + numString;
+        return result;
+    }
+
+    public int compareAbsValue(BigNum num) {
+        int result = numString.length() - num.numString.length();
+
+        if (result == 0) {
+            result = numString.compareTo(num.numString);
+        }
+
+        return result;
     }
 }
--- AdditionAndSubtraction/src/welp/poc/Main.java
+++ AdditionAndSubtraction/src/welp/poc/Main.java
@@ -8,56 +8,58 @@ public class Main
     public static void main(String[] args) {
         System.out.println("Big Number Calculator");
 
-        BigNum num1 = new BigNum("-345");
-        BigNum num2 = new BigNum("-495");
+        BigNum num1 = new BigNum("-567");
+        BigNum num2 = new BigNum("567");
+        BigNum num3 = new BigNum("0");
 
         StringBuilder sb = new StringBuilder();
 
         int maxLen = Math.max(num1.numString.length(), num2.numString.length());
         if(num1.numSign == num2.numSign) {
-            if (num1.numSign == "+") {
-                add(sb, maxLen, num1.numString, num2.numString);
-            }
+            add(sb, maxLen, num1.numString, num2.numString);
+            num3.setValue("+", sb.toString());
             if (num1.numSign == "-") {
-                add(sb, maxLen, num1.numString, num2.numString);
-                sb.insert(0, "-");
+                num3.switchSign();
             }
         } else {
-            BigNum largerNum = getLargerNum(num1, num2);
-            BigNum smallerNum = num2;
-            if(largerNum == num2){
-                smallerNum = num1;
+            if(num1.compareAbsValue(num2) >= 0){
+                subtract(sb, maxLen, num1.numString, num2.numString);
+                num3.setValue(num1.numSign, sb.toString());
+            } else {
+                subtract(sb, maxLen, num2.numString, num1.numString);
+                num3.setValue(num2.numSign, sb.toString());
             }
         }
+
         String sign1 = num1.numSign;
         String sign2 = num2.numSign;
         if(sign1 == "+"){
             sign1 = "";
         }
-        System.out.format("%s%s %s %s = %s\n", sign1, num1.numString, sign2, num2.numString, sb.toString());
+        System.out.format("%s%s %s %s = %s\n", sign1, num1.numString, sign2, num2.numString, num3.getValue());
     }
 
-    public static BigNum getLargerNum(BigNum num1, BigNum num2){
-        BigNum result = num1;
-        if(num2.numString.length() > num1.numString.length()) {
-            result = num2;
-        }
-        else if(num1.numString.length() == num2.numString.length()){
-            for(int i=0; i<num1.numString.length(); i++){
-                if(Character.getNumericValue(num2.numString.charAt(i))>Character.getNumericValue(num1.numString.charAt(i))){
-                    result = num2;
-                    break;
-                } else if(Character.getNumericValue(num1.numString.charAt(i))>Character.getNumericValue(num2.numString.charAt(i))){
-                    break;
-                }
-            }
-        }
-        return result;
-    }
+//    public static BigNum getLargerNum(BigNum num1, BigNum num2){
+//        BigNum result = num1;
+//        if(num2.numString.length() > num1.numString.length()) {
+//            result = num2;
+//        }
+//        else if(num1.numString.length() == num2.numString.length()){
+//            for(int i=0; i<num1.numString.length(); i++){
+//                if(Character.getNumericValue(num2.numString.charAt(i))>Character.getNumericValue(num1.numString.charAt(i))){
+//                    result = num2;
+//                    break;
+//                } else if(Character.getNumericValue(num1.numString.charAt(i))>Character.getNumericValue(num2.numString.charAt(i))){
+//                    break;
+//                }
+//            }
+//        }
+//        return result;
+//    }
 
     public static void add(StringBuilder sb, int maxLen, String num1, String num2){
         int carry = 0;
-        for (int i = 0; i < maxLen; i++) {
+        for (int i = 0; i < maxLen+1; i++) {
             int a1 = 0;
             int a2 = 0;
             if (i < num1.length()) {
@@ -78,6 +80,7 @@ public class Main
 
     public static void subtract(StringBuilder sb, int maxLen, String largerNum, String smallerNum){
         int carry = 0;
+        int diff;
         for (int i = 0; i < maxLen; i++) {
             int a1 = 0;
             int a2 = 0;
@@ -87,8 +90,13 @@ public class Main
             if (i < smallerNum.length()) {
                 a2 = Character.getNumericValue(smallerNum.charAt(smallerNum.length()-1-i));
             }
-            int diff = (a1 - a2 - carry+ 10) % 10;
-            carry = (int) ((a1 + a2 + carry) / 10);
+            if(a1-a2-carry >= 0){
+                diff = a1-a2-carry;
+                carry = 0;
+            } else {
+                diff = a1+10-a2-carry;
+                carry = 1;
+            }
             sb.insert(0, String.valueOf(diff));
         }
     }
